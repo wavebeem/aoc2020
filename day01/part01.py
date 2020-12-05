3465e70a5bde067b967e41cde870e66dfca542b8
Brian Mock <brian@wavebeem.com>
2020-12-04 23:45:25 -0800

    i'll just stick with js, thx

--- day01/part01.py
+++
@@ -1,8 +0,0 @@
-import os
-
-with open("day01/input.txt") as input_file:
-    input = [int(line) for line in input_file.readlines()]
-    for (i, n) in enumerate(input):
-        for m in input[i:]:
-            if n + m == 2020:
-                print(n * m)
--- day01/part01.rb
+++
@@ -1,8 +0,0 @@
-input = File.readlines("day01/input.txt").map(&:to_i)
-input.each_with_index do |n, i|
-  input[i..].each do |m|
-    if n + m == 2020
-      puts(n * m)
-    end
-  end
-end
