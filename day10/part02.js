d5cbdb8344ceca9b8a41f0e3376fb4a875f54285
Brian Mock <brian@wavebeem.com>
2020-12-09 23:56:13 -0800

    idk, idk

--- day10/part02.js
+++ day10/part02.js
@@ -1,3 +1,16 @@
+// We found out in part 1 that we can use _every_ adapter in a giant chain.
+// That's our longest chain. Any other chain must be shorter, since the order
+// must always be increasing. Given that, it seems like every "optional" adapter
+// is a decision point that multiplies the number of routes available by 2. By
+// optional I mean that in this sequence: 0, 1, 2, 3, 6 you could shorten it to
+// 0, 3, 6 since each step is allowed to increase by a maximum of 3. Such an
+// would need to be some power of 2 multiplied by the number of optional
+// elements. For the solution with 19208 options, this could be at best 8 *
+// 2401, and 2401 vastly exceeds the input size. So I guess that's probably
+// wrong.
+
+// Comments online suggested caching/memoization would be useful here.
+
 const fs = require("fs");
 
 const input = fs
