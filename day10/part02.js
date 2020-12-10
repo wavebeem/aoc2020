d7de504a92f837dfd0b8f5a4c59183c686c24436
Brian Mock <brian@wavebeem.com>
2020-12-09 23:16:01 -0800

    idk

--- day10/part02.js
+++ day10/part02.js
@@ -8,18 +8,34 @@ const input = fs
   .sort((a, b) => Math.sign(a - b));
 
 const target = input[input.length - 1] + 3;
-
-function countPaths(input, start, end) {
-  if (input.length === 0) {
-    return end - start <= 3 ? 1 : 0;
-  }
-  let sum = 0;
-  for (let i = 0; i < 3; i++) {
-    if (input[i] - start <= 3) {
-      sum += countPaths(input.slice(i + 1), input[i], end);
+const allJolts = [0, ...input, target];
+const nodeByJolt = new Map();
+for (const jolt of allJolts) {
+  nodeByJolt.set(jolt, { jolt });
+}
+for (const jolt of allJolts) {
+  const node = nodeByJolt.get(jolt);
+  for (let offset of [1, 2, 3]) {
+    const otherNode = nodeByJolt.get(jolt - offset);
+    if (otherNode) {
+      otherNode["plus" + offset] = node;
     }
   }
-  return sum;
 }
 
-console.log(countPaths(input, 0, target));
+function countRoutes(node, target) {
+  if (!node) {
+    return 0;
+  }
+  if (node.jolt === target) {
+    return 1;
+  }
+  return (
+    countRoutes(node.plus1, target) +
+    countRoutes(node.plus2, target) +
+    countRoutes(node.plus3, target)
+  );
+}
+
+const root = nodeByJolt.get(0);
+console.log(countRoutes(root, target));
