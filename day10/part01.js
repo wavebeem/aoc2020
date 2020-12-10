f109fc4395787fcfd7f7edba1c2bb8e9253f48bc
Brian Mock <brian@mockbrian.com>
2020-12-09 21:45:15 -0800

    day10/part01

---
+++ day10/part01.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+const input = fs
+  .readFileSync("day10/input.txt", "utf-8")
+  .trim()
+  .split("\n")
+  .map(Number)
+  .sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
+
+input.push(input[input.length - 1] + 3);
+
+let currentJoltage = 0;
+let countJoltDiff1 = 0;
+let countJoltDiff3 = 0;
+for (const jolt of input) {
+  const diff = jolt - currentJoltage;
+  if (diff === 1) {
+    countJoltDiff1++;
+  } else if (diff === 3) {
+    countJoltDiff3++;
+  }
+  currentJoltage = jolt;
+}
+
+console.log(countJoltDiff1 * countJoltDiff3);
