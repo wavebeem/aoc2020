70b48a06174583c53a27869684c042f58f3961a7
Brian Mock <brian@wavebeem.com>
2020-12-13 20:29:45 -0800

    day13/part01

---
+++ day13/part01.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("day13/input.txt", "utf-8").trim().split("\n");
+
+const start = Number(input[0]);
+const buses = input[1]
+  .split(",")
+  .filter((b) => b !== "x")
+  .map(Number)
+  .sort((a, b) => Math.sign(a - b));
+
+function findFirstBus(start, buses) {
+  let time = start;
+  while (true) {
+    for (const bus of buses) {
+      if (time % bus === 0) {
+        return { bus, time };
+      }
+    }
+    time++;
+  }
+}
+
+const { bus, time } = findFirstBus(start, buses);
+console.log((time - start) * bus);
